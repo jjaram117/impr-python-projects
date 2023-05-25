10eaa1f4f0b8519fe41e29279cd39322bd23be38
Jared J <jjaram@kali.kalilinux>
2023-05-25 11:31:46 -0400

    first commit

---
+++ 1-Name_Generator/README.txt
@@ -0,0 +1,3 @@
+#Lesson 1 - Silly Name Generator based on Psych TV Show. 
+
+#Python PEP 8 and PEP 257 style guides. Pylint and Pydocstyle also used
---
+++ 1-Name_Generator/main.py
@@ -0,0 +1,202 @@
+#Lesson 1 - Silly Name Generator based on Psych TV Show. 
+
+#Python PEP 8 and PEP 257 style guides. Pylint and Pydocstyle also used
+
+#Goal: Utilize Python and create a program that generates sill names while conforming to established style guidelines
+
+
+#---------------------------------------------------------------------------------------------------------#
+#Initial Attempt at writing the code before reading the instructions
+
+
+##---- MOST BASIC ATTEMPT
+#1. Creating/importing first and last names
+#Hardcoding names from book's GitHub - https://github.com/rlvaugh/Impractical_Python_Projects/tree/master/Chapter_1 :
+firstName = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms')
+
+lastName = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')
+
+
+#2. Randomly selecting a first and last name
+#using random.choice 
+import random
+first = random.choice(firstName)
+last = random.choice(lastName)
+
+
+#3. Outputting the result of the RNG full name
+# print("My name is " + first + " " + last + " and I, hate, taxeeeeees")
+# print("My name is " + random.choice(firstName) + " " + random.choice(lastName) + " and I, hate, taxeeeeees")
+
+
+
+##----ADAPTING ABOVE CODE TO GENERATE NAME BASED ON LETTER INTITIAL INPUT
+import random
+import sys
+
+
+def checkValidChar(firstInitial, lastInitial):   #Checks that initial is actually a letter 
+	#Return error details if initial inputs are not valid letters
+	if (firstInitial.isalpha() != True) and (lastInitial.isalpha() != True):
+		print('Both of your inputs are invalid. Please use only LETTERS from the english language')
+		exit()		
+
+	elif (firstInitial.isalpha() != True):
+		print('Your first initial input is invalid. Please use only LETTERS from the english language')
+		exit()
+
+	elif (lastInitial.isalpha() != True):
+		print('Your last initial input is invalid. Please use only LETTERS from the english language')
+		exit()
+
+	else:
+		return
+
+
+def generateName(firstInitial, lastInitial): #Main Function
+	#Hardcode Names
+	firstName = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms')
+	lastName = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')	
+
+	#1. Return an error if name tuple does not possess names with input initials. Kill rest of script after this 
+	checkValidChar(firstInitial, lastInitial)
+
+	#2.A. Determine eligible first names based on "firstInitial" input 
+	eligibleFirstNames = []
+	for i in range(len(list(firstName))):
+		if firstName[i][0] == firstInitial:
+			eligibleFirstNames.append(firstName[i])
+
+	if len(eligibleFirstNames) == 0:
+		print("Unfortunately, no name in the database exists with the starting initial '" + firstInitial + "'. Please try another letter.")
+		exit()
+	
+
+	#2.B. Determine eligible last names based on "lastInitial" input 
+	eligibleLastNames = []
+	for i in range(len(list(lastName))):
+		if lastName[i][0] == lastInitial:
+			eligibleLastNames.append(lastName[i])
+
+	if len(eligibleLastNames) == 0:
+			print("Unfortunately, no last name in the database begins with the starting initial '" + lastInitial + "'. Please try another letter.")
+			exit()
+
+
+	#3. Randomize based on the selected initials and output the new RNG name
+	# first = random.choice(eligibleFirstNames)
+	# last = random.choice(eligibleLastNames)
+	# print("My name is " + first + " " + last + " and I, hate, taxeeeeees")
+	print("My name is " + random.choice(eligibleFirstNames) + " " + random.choice(eligibleLastNames) + " and I'm your RNG name, you psycho")
+
+	return 
+
+# generateName("B", "J")
+
+
+
+
+#---------------------------------------------------------------------------------------------------------#
+#NOW finally following along with the lesson
+
+import sys, random
+firstName = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms')
+
+lastName = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')
+
+print("Welcome to your automatic name generator!\n")
+
+while True:
+	firstName = random.choice(firstName)
+	lastName = random.choice(lastName)
+	
+	print("\n\n")
+	print("{} {}".format(firstName, lastName),file=sys.stderr)
+	print("\n\n")
+
+	tryAgain = input("\n\nDo you want to try again? (Press Enter to tray again, else press n to quit)")
+	if tryAgain.lower() == "n":
+		break
+
+input("\nPress Enter to Exit.")
